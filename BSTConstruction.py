2c7c2326a2da20bbd1db46b87e601edb3157aded
Rachit <rachit2311@gmail.com>
2019-01-22 20:11:36 +0530

    Initial Commit : BST Construction

---
+++ BSTConstruction.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
+        return self
+
+    def contains(self, value):
+        # Write your code here.
+
+    def remove(self, value):
+        # Write your code here.
+        # Do not edit the return statement of this method.
+        return self
+
+		
+if __name__ == '__main__':
+	pass
