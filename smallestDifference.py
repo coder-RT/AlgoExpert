cabb435f1af6d8d1ff4b18d37d70cd3dbaaacc21
Rachit <rachit2311@gmail.com>
2019-02-04 08:22:05 +0530

    initial version|closestNumber

---
+++ smallestDifference.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+#Time O(NlogN + MlogM) | Space O(1)
+def smallestDifference(first_array, second_array):
+    # Write your code here.
+	
+	
+	
+		
+if __name__ == '__main__':
+	first_array = [-1, 5, 10, 20, 28, 3]
+	second_array = [26, 134, 135, 15, 17]
+	result = smallestDifference(first_array, second_array)
+	print(result)
