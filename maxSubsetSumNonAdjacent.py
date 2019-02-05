e89968a9cc86aba6ee0daacb913383ed85afc2b7
Rachit <rachit2311@gmail.com>
2019-02-05 23:39:05 +0530

    subsequent commit

--- maxSubsetSumNonAdjacent.py
+++ maxSubsetSumNonAdjacent.py
@@ -1,8 +1,21 @@
-#code
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
 
 # find the max subset sum : non adjacent values
+
+#Time O(N) | Space O(N)
 def maxSubsetSum(array):
-    pass
+    # if array is empy
+	if not len(array):
+		return []
+	#if array has only one element
+	elif len(array) == 1:
+		return array[0]
+	# base condition
+	maxSum = array[:]
+	maxSum[1] = max(array[0], array[1])
+	for i in range(2, len(array)):
+		maxSum[i] = max ()
 
 		
 if __name__ == '__main__':
