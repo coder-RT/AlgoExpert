432280f0cc36fbed585d2f0aa9e5a7ba9279fe52
Rachit <rachit2311@gmail.com>
2019-02-05 23:22:53 +0530

    Initial commit | maxSubsetSumNonAdjacent

---
+++ maxSubsetSumNonAdjacent.py
@@ -0,0 +1,11 @@
+#code
+
+# find the max subset sum : non adjacent values
+def maxSubsetSum(array):
+    pass
+
+		
+if __name__ == '__main__':
+array = [75, 105, 120, 75, 90, 135]
+result = maxSubsetSum(array)
+print(result)
