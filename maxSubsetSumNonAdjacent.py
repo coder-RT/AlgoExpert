564562be3e48cf19657f49d84af45f18466f4d36
Rachit <rachit2311@gmail.com>
2019-02-05 23:43:34 +0530

    Time: O(N)|Space: O(N)maxSubsetSumNonAdjacentValues

--- maxSubsetSumNonAdjacent.py
+++ maxSubsetSumNonAdjacent.py
@@ -7,7 +7,7 @@
 def maxSubsetSum(array):
     # if array is empy
 	if not len(array):
-		return []
+		return None
 	#if array has only one element
 	elif len(array) == 1:
 		return array[0]
@@ -15,10 +15,10 @@ def maxSubsetSum(array):
 	maxSum = array[:]
 	maxSum[1] = max(array[0], array[1])
 	for i in range(2, len(array)):
-		maxSum[i] = max ()
+		maxSum[i] = max (maxSum[i-1], maxSum[i-2] + array[i])
+	return maxSum[-1]
 
-		
 if __name__ == '__main__':
-array = [75, 105, 120, 75, 90, 135]
-result = maxSubsetSum(array)
-print(result)
+	array = [75, 105, 120, 75, 90, 135]
+	result = maxSubsetSum(array)
+	print(result)
