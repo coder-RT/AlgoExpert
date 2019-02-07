ce9a6e12e5c5b76fdabb2b5683b261b1c2e6e362
Rachit <rachit2311@gmail.com>
2019-02-07 19:55:49 +0530

    Dynamic Programming || Coin Change Problem

---
+++ coinChange.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+def denominationArray(denominationArray, target):
+	pass
+
+if __name__ == '__main__':
+	denominationArray = [1, 2, 5, 10]
+	target = 10
+	result = coinChange(denominationArray, target)
+	print(result)
