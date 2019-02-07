2b085e158d1ec8356d70cd8c82730b74d91573e8
Rachit <rachit2311@gmail.com>
2019-02-07 20:09:26 +0530

    Time:O(ND)|Space:O(N)||Dynamic Programming||Coin Change Problem

--- coinChange.py
+++ coinChange.py
@@ -1,11 +1,17 @@
 #! /usr/bin/python
 #-*- coding: utf-8 -*-
 
-def denominationArray(denominationArray, target):
-	pass
+def coinChange(denominationArray, target):
+	ways = [0] * len(range(target+1))
+	ways[0] = 1
+	for denomination in denominationArray:
+		for amount in range(len(ways)):
+			if denomination <= amount: 
+				ways[amount] = ways[amount] + ways[amount-denomination]
+	return ways[-1]
 
 if __name__ == '__main__':
-	denominationArray = [1, 2, 5, 10]
+	denominationArray = [1, 5, 10, 25]
 	target = 10
 	result = coinChange(denominationArray, target)
 	print(result)
