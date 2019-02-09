579e2d2c57a78389cfa4a94d99b8b9bc45b09540
Rachit <rachit2311@gmail.com>
2019-02-09 16:12:09 +0530

    LevenshteinDistance:InitialCommit

---
+++ LevenshteinDistance.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+#-*- coding: utf-8 -*-
+
+def calculateMinimumDistance(str1, str2):
+    pass
+
+if __name__ == '__main__':
+	str1 = "abc"
+	str2 = "yabd"
+	result = calculateMinimumDistance(str1, str2)
+	print(result)
