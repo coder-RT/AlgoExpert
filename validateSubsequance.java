4bacfbddfecbba1871d78e2fa47324006a945e94
Rachit <rachit2311@gmail.com>
2020-12-27 17:27:04 +0530

    validateSubsequence : initial commit

---
+++ validateSubsequance.java
@@ -0,0 +1,8 @@
+public class validateSubsequance {
+    public static boolean validateSubseqence(int[] array, int[] subsequence){
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(validateSubseqence(new int[] {5,1,22,25,6,-1,8,10}, new int[] {1,6,-1,10}));
+    }
+}
