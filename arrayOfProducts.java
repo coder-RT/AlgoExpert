915ba4201fd182f3f1d8f5a71031af184f186765
Rachit <rachit2311@gmail.com>
2020-12-27 22:20:56 +0530

    Time: O(N)| Space: O(N)

--- arrayOfProducts.java
+++ arrayOfProducts.java
@@ -2,7 +2,26 @@ import java.util.Arrays;
 
 public class arrayOfProducts {
     public static int[] arrayOfProducts(int[] array){
-        return new int[] {1,1,1,1};
+        int[] products = new int[array.length];
+        int[] lftProducts = new int[array.length];
+        int[] rgtProducts = new int[array.length];
+
+        int lftRunningProduct = 1;
+        for(int i=0; i < array.length; i++){
+            lftProducts[i] = lftRunningProduct;
+            lftRunningProduct *= array[i];
+        }
+
+        int rgtRunningProduct = 1;
+        for(int i=array.length-1; i >= 0; i--){
+            rgtProducts[i]  = rgtRunningProduct;
+            rgtRunningProduct *= array[i];
+        }
+
+        for(int i=0; i<array.length; i++){
+            products[i] = lftProducts[i] * rgtProducts[i];
+        }
+        return products;
     }
     public static void main(String[] args){
         System.out.println(Arrays.toString(arrayOfProducts(new int[] {5,1,4,2})));
