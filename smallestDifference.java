2dec06cdb2a36d0098b2b6d5193f79ef1d7abf42
Rachit <rachit@Rachits-MacBook-Pro.local>
2020-12-27 16:20:36 +0530

    Time: O(Nlog(N)+M(log(M))| Space: O(1)

--- smallestDifference.java
+++ smallestDifference.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+
 public class smallestDifference {
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo){
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+        int idxOne = 0;
+        int idxTwo = 0;
+        int current = Integer.MAX_VALUE;
+        int smallest = Integer.MAX_VALUE;
+        int[] smallestPair = new int[2];
+        while (idxOne < arrayOne.length && idxTwo < arrayTwo.length){
+            int firstNum = arrayOne[idxOne];
+            int secondNum = arrayTwo[idxTwo];
+
+            if (firstNum < secondNum){
+                current = secondNum - firstNum;
+                idxOne++;
+            }
+            else if (secondNum < firstNum){
+                current  = firstNum - secondNum;
+                idxTwo++;
+            }
+            else {
+                return new int[] {firstNum, secondNum};
+            }
+            if (current < smallest){
+                smallest = current;
+                smallestPair = new int[] {firstNum, secondNum};
+            }
+        }
+        return smallestPair;
+    }
     public static void main (String[] args){
-        System.out.println("hello world!!");
+        System.out.println(Arrays.toString(smallestDifference(new int[]{-1,3,5,10,20,28},new int[]{15,17,26,134,135})));
     }
 }
