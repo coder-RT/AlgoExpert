9095fdc58b208a2f5184d8e6787841f0c0e2284c
Rachit <rachit2311@gmail.com>
2020-12-27 17:36:30 +0530

    Time: O(N)|Space: O(1)

--- validateSubsequance.java
+++ validateSubsequance.java
@@ -1,6 +1,16 @@
 public class validateSubsequance {
     public static boolean validateSubseqence(int[] array, int[] subsequence){
-        return true;
+        int arrIdx = 0;
+        int seqIdx = 0;
+
+        while(arrIdx < array.length && seqIdx < subsequence.length){
+            if(array[arrIdx] == subsequence[seqIdx]){
+                seqIdx++;
+            }
+            arrIdx++;
+        }
+
+        return seqIdx == subsequence.length;
     }
     public static void main(String[] args){
         System.out.println(validateSubseqence(new int[] {5,1,22,25,6,-1,8,10}, new int[] {1,6,-1,10}));
