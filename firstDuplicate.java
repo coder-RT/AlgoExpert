e8789cece51f2ef068a8ae4e03bce34b80700094
Rachit <rachit2311@gmail.com>
2020-12-29 14:21:10 +0530

    Time:O(N)|Space:O(1)

--- firstDuplicate.java
+++ firstDuplicate.java
@@ -1,5 +1,11 @@
 public class firstDuplicate {
     public static int firstDuplicate(int[] array){
+        for(int i=0; i<array.length; i++){
+            if(array[Math.abs(array[i]) - 1] < 0){
+                return Math.abs(array[i]);
+            }
+            array[Math.abs(array[i])-1] *= -1;
+        }
         return -1;
     }
     public static void main(String args[]){
