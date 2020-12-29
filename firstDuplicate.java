17d8e64093ba544e90ed7d46e73bbd3a742178c3
Rachit <rachit2311@gmail.com>
2020-12-29 13:43:42 +0530

    firstDuplicate : initial Commit

---
+++ firstDuplicate.java
@@ -0,0 +1,8 @@
+public class firstDuplicate {
+    public static int firstDuplicate(int[] array){
+        return -1;
+    }
+    public static void main(String args[]){
+        System.out.println(firstDuplicate(new int[] {2,1,5,3,3,2,4}));
+    }
+}
